d9f97fd16fe109496d2aec17dbecd0d5670cb2a2
Anatoliy <avkindrat2006@gmail.com>
2025-11-11 21:26:46 +0200

    Add subtract and multipy functions

--- Calculator.py
+++ Calculator.py
@@ -7,6 +7,17 @@ diya=input("Enter the operation (+, -, *, /):")
 def add(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
 if diya == "+":
     print("The sum is:", add(a, b))
 
+elif diya == "-":
+    print("The difference is:", subtract(a, b))
+
+elif diya == "*":
+    print("The product is:", multiply(a, b))
