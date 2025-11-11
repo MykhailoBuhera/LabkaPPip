a46598a107e930dd1f840e188c2766d39ee28821
Mykhailo Buhera <mihapvp1990@gmail.com>
2025-11-11 20:42:49 +0200

    first commit from the best TM

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+a = 0
+b = 0
+diya = ""
+a=input("Enter first number:")
+b=input("Enter second number:")
+diya=input("Enter the operation (+, -, *, /):")
+def add(a, b):
+    return a + b
+
+if diya == "+":
+    print("The sum is:", add(a, b))
+
